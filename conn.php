5c0fe8a9ccef1e1c00e58c691c581327c83a5aff
br <105134757+shundebo@users.noreply.github.com>
2022-12-26 14:35:41 +0800

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,8 +1,8 @@
 <?php
 // Change this to your own configuration
-$link = new mysqli('localhost','NAME','PASSWORD','kano');
+$link = new mysqli('localhost','NAME','PASSWORD','penguin');
 mysqli_set_charset($link, 'utf8');
 if ($link->connect_error) {
     die("Failed to connect: " . $conn->connect_error);
 }
-$ranking = "kano_rank";
+$ranking = "mc_rank";
